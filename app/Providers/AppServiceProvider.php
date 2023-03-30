119dd3c5a350e3a36bc1f99c13b30437f45830da
Nerijus <krekas@gmail.com>
2023-03-30 14:35:07 +0300

    Init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Filament\Resources\CourseResource;
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Model::unguard();
+
+        Filament::serving(function () {
+            Filament::registerNavigationItems([
+                NavigationItem::make('Courses')
+                    ->url(CourseResource::getUrl('index'))
+                    ->icon('heroicon-o-academic-cap')
+                    ->activeIcon('heroicon-s-academic-cap')
+                    ->isActiveWhen(fn (): bool => request()->routeIs('filament.resources.courses.*') || request()->routeIs('filament.resources.courses/lessons.*')),
+            ]);
+        });
+    }
+}
